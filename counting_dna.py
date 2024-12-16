1e57edc72ae5aff1cf2c408b2ce0d4766a86ad4f
Kelson Wysocki <32180124+Kotuon@users.noreply.github.com>
2024-12-16 09:33:15 -0800

    Initial commit.

---
+++ counting_dna.py
@@ -0,0 +1,18 @@
+
+countArray = [0] * 4
+
+def parseInput(fileStr) :
+    countArray[0] = fileStr.count("A")
+    countArray[1] = fileStr.count("C")
+    countArray[2] = fileStr.count("G")
+    countArray[3] = fileStr.count("T")
+
+file_name = input("Enter file name: ")
+print(file_name)
+
+file = open(file_name, "r")
+fileData = file.read()
+
+parseInput(fileData)
+
+print(str(countArray[0]) + " " + str(countArray[1]) + " " + str(countArray[2]) + " " + str(countArray[3]))
