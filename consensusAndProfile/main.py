2be44cc5e978fc1a95a05e9328e181a79fd0d87d
Kelson Wysocki <32180124+Kotuon@users.noreply.github.com>
2025-01-07 09:05:02 -0800

    Finished next problem
    ### The problem
    For this Rosalind problem I am supposed to take several DNA sequences (FASTA format) and determine what the consensus string is (most common symbol at each position)
    
    ### Solution
    For reading the input I used my previously made FASTA parser which stores each DNA string in a list of FastaSequence objects.
    
    Next, I create a dictionary of keys 'A', 'C', 'G', 'T' each with a list of size sequenceLength of zeros. Using this dictionary, I simultaneously iterate through the DNA sequences and increment the count if a key appears at a position.
    
    Once I've gone through the whole sequence I determine the max key at each position, generate the consensus string, and output the string and table.

--- consensusAndProfile/main.py
+++ consensusAndProfile/main.py
@@ -3,15 +3,38 @@ import sys
 sys.path.append('../rosalind_files')
 
 import filereader  # nopep8
+import fastaParser  # nopep8
 
 
 def main():
     fileName = input("Enter file name: ")
 
     file = filereader.getFile(fileName)
-    fileData = file.read()
 
-    print(fileData)
+    dnaSequences = fastaParser.parse(file)
+    sequenceLength = len(dnaSequences[0].sequence)
+
+    # ACGT
+    keys = ['A', 'C', 'G', 'T']
+
+    table = dict((keys[i], [0 for j in range(sequenceLength)])
+                 for i in range(0, 4))
+
+    for i in range(0, sequenceLength):
+        for line in dnaSequences:
+            table[line.sequence[i]][i] += 1
+
+    output = ""
+    for i in range(0, sequenceLength):
+        currMax = ('',  -1)
+        for key in keys:
+            if table[key][i] > currMax[1]:
+                currMax = (key, table[key][i])
+        output += currMax[0]
+
+    print(output)
+    for k, v in table.items():
+        print(k + ": " + ' '.join(map(str, v)))
 
 
 main()
