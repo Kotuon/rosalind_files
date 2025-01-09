876b836027469d3f3d705e6e858c9b98fe7ff27c
Kelson Wysocki <32180124+Kotuon@users.noreply.github.com>
2025-01-09 08:04:53 -0800

    Initial commit

---
+++ overlapGraphs/main.py
@@ -0,0 +1,17 @@
+
+import sys
+sys.path.append('../rosalind_files')
+
+import filereader  # nopep8
+
+
+def main():
+    fileName = input("Enter file name: ")
+
+    file = filereader.getFile(fileName)
+    fileData = file.read().replace("\n", "")
+
+    print(fileData)
+
+
+main()
