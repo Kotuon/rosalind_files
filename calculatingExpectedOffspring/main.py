fbe0a7838bdcdb162fd2830d3ea6e5785ee5a1ec
Kelson Wysocki <32180124+Kotuon@users.noreply.github.com>
2025-01-15 08:56:01 -0800

    Finished next problem

--- calculatingExpectedOffspring/main.py
+++ calculatingExpectedOffspring/main.py
@@ -5,13 +5,23 @@ sys.path.append('../rosalind_files')
 import filereader  # nopep8
 
 
+evMatrix = [1, 1, 1, 3/4, 1/2, 0]
+offspringCount = 2
+
+
 def main():
     fileName = input("Enter file name: ")
 
     file = filereader.getFile(fileName)
-    fileData = file.read()
+    fileData = file.read().replace("\n", "")
 
-    print(fileData)
+    values = list(map(int, fileData.split(' ')))
+
+    result = 0
+    for i in range(len(values)):
+        result += evMatrix[i] * offspringCount * values[i]
+
+    print(result)
 
 
 main()
